a67da0bbdc13fecc3a1bee41425f5f8b980fa41b
Fredrik Gullberg <fredrik.gullberg@klarna.com>
2023-03-03 10:36:41 +0100

    feat(cli): use dotenv for cli
    CFP-0023

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-
 import chalk from 'chalk'
+import * as dotenv from 'dotenv'
 import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs/yargs'
 
-import { check } from './check'
+import { check } from './check' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 // eslint-disable-next-line no-unused-expressions
 yargs(hideBin(process.argv))
